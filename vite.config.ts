ffd06a635b0871a2611e07d42beea35c26d0b22a
dickwin2003 <dickwin2003@gmail.com>
2024-11-23 11:35:02 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -23,4 +24,9 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./app"),
+    },
+  },
 });
