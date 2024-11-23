613f6e3a8a2be3a71e265c73cda29a7d236cb850
dickwin2003 <dickwin2003@gmail.com>
2024-11-23 11:43:28 +0800

    11:37:25.522 Error: Failed to publish your Function. Got error: Uncaught Error: @remix-run/dev/server-build is not meant to be used directly from node_modules. It exists to provide type definitions for a virtual module provided by the Remix compiler at build time.   at functionsWorker-0.21687750382850202.js:7707:11 in ../node_modules/@remix-run/dev/server-build.js   at functionsWorker-0.21687750382850202.js:11:50 in __require   at functionsWorker-0.21687750382850202.js:7736:21 in _middleware.ts   at functionsWorker-0.21687750382850202.js:8:56 in __init   at functionsWorker-0.21687750382850202.js:7750:5 in ../../../pages/build_tool/js/.wrangler/tmp/pages-Gsf5PP/functionsRoutes-0.41389214271986297.mjs   at functionsWorker-0.21687750382850202.js:8:56 in __init   at functionsWorker-0.21687750382850202.js:7764:1

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,5 +1,5 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "@remix-run/dev/server-build";
+import * as build from "../build/server/index.js";
 
 const handleRequest = createPagesFunctionHandler({
   build,
